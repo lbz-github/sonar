3e19ec29b8554b0b238f4db822980bc17064c8eb
lbz-github <56929122+lbz-github@users.noreply.github.com>
2022-09-23 13:43:53 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -10,7 +10,7 @@ enum PingUnit {
 /**
  * Sonar and ping utilities
  */
-//% color="#26A65B" weight=10
+//% color="#FF543B" weight=10
 namespace sonar {
     /**
      * Send a ping and get the echo time (in microseconds) as a result
@@ -26,7 +26,7 @@ namespace sonar {
         pins.digitalWritePin(trig, 0);
         control.waitMicros(2);
         pins.digitalWritePin(trig, 1);
-        control.waitMicros(20);
+        control.waitMicros(25);
         pins.digitalWritePin(trig, 0);
 
         // read pulse
